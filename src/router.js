1253efb883503670aaab7981710ca9ede845a40e
Natalija Ojdanic <natalija.ojdanic@gmail.com>
2019-07-21 11:16:33 +0200

    Lekcija 1,2,3,4

---
+++ src/router.js
@@ -0,0 +1,32 @@
+import Vue from "vue";
+import Router from "vue-router";
+import EventList from "./views/EventList.vue";
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "event-list",
+      component: EventList
+    },
+    {
+      path: "/event/:id",
+      name: "event-show",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/EventShow.vue"),
+      props: true
+    },
+    {
+      path: "/event/create",
+      name: "event-create",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/EventCreate.vue")
+    }
+  ]
+});
