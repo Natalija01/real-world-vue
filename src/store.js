bc208a69e6a4b60cf736fe0a05d18966c8484c48
Natalija Ojdanic <natalija.ojdanic@gmail.com>
2019-07-24 15:48:36 +0200

    Mastering Vuex - Lekcija 5 - Mutatation and action 2

--- src/store.js
+++ src/store.js
@@ -16,11 +16,23 @@ export default new Vuex.Store({
       "food",
       "community"
     ],
-    events: []
+    events: [],
+    eventsTotal: 0,
+    event: {}
   },
+  /*Mutation set SET_EVENTS  state*/
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event);
+    },
+    SET_EVENTS(state, events) {
+      state.events = events;
+    },
+    SET_EVENTS_TOTAL(state, eventsTotal) {
+      state.eventsTotal = eventsTotal;
+    },
+    SET_EVENT(state, event) {
+      state.event = event;
     }
   },
   actions: {
@@ -28,6 +40,35 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit("ADD_EVENT", event);
       });
+    },
+    /*commit is context objext, and response.data is payload,which is list of events*/
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getEvents(perPage, page)
+        .then(response => {
+          commit(
+            "SET_EVENTS_TOTAL",
+            parseInt(response.headers["x-total-count"])
+          );
+          commit("SET_EVENTS", response.data);
+        })
+        .catch(error => {
+          console.log("There was an error" + error.response);
+        });
+    },
+    fetchEvent({ commit, getters }, id) {
+      var event = getters.getEventById(id);
+
+      if (event) {
+        commit("SET_EVENT", event);
+      } else {
+        EventService.getEvent(id)
+          .then(response => {
+            commit("SET_EVENT", response.data);
+          })
+          .catch(error => {
+            console.log("There was an error" + error.response);
+          });
+      }
     }
   },
   getters: {
