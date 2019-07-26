0bc433e0fb6855b28806d024312416b9c144de7e
Natalija Ojdanic <natalija.ojdanic@gmail.com>
2019-07-26 09:51:08 +0200

    Mastering Vuex - Lekcija 7 - Notification errors, success

---
+++ src/store/modules/event.js
@@ -0,0 +1,90 @@
+import EventService from "@/services/EventService.js";
+
+export const namespaced = true;
+export const state = {
+  events: [],
+  eventsTotal: 0,
+  event: {}
+};
+/*Mutation set SET_EVENTS  state*/
+export const mutations = {
+  ADD_EVENT(state, event) {
+    state.events.push(event);
+  },
+  SET_EVENTS(state, events) {
+    state.events = events;
+  },
+  SET_EVENTS_TOTAL(state, eventsTotal) {
+    state.eventsTotal = eventsTotal;
+  },
+  SET_EVENT(state, event) {
+    state.event = event;
+  }
+};
+export const actions = {
+  createEvent({ commit, dispatch }, event) {
+    return EventService.postEvent(event)
+      .then(() => {
+        commit("ADD_EVENT", event);
+
+        const notification = {
+          type: "success",
+          message: "Event was created"
+        };
+
+        dispatch("notification/add", notification, { root: true });
+      })
+      .catch(error => {
+        const notification = {
+          type: "error",
+          message: "There was a problem creating your event" + error.message
+        };
+        dispatch("notification/add", notification, { root: true });
+        throw error;
+      });
+  },
+  /*commit is context objext, and response.data is payload,which is list of events*/
+  fetchEvents({ commit, dispatch }, { perPage, page }) {
+    EventService.getEvents(perPage, page)
+      .then(response => {
+        commit("SET_EVENTS_TOTAL", parseInt(response.headers["x-total-count"]));
+        commit("SET_EVENTS", response.data);
+      })
+      .catch(error => {
+        const notification = {
+          type: "error",
+          message: "There was a problem fetching events" + error.message
+        };
+        /* dispatch allows us to send add action from notification 
+        module and pass notification object*/
+        /* root: true alows dispatch to go to the root state find a notification 
+        module and add 'add' action $store>notification>add */
+        dispatch("notification/add", notification, { root: true });
+      });
+  },
+  fetchEvent({ commit, getters, dispatch }, id) {
+    var event = getters.getEventById(id);
+
+    if (event) {
+      commit("SET_EVENT", event);
+    } else {
+      EventService.getEvent(id)
+        .then(response => {
+          commit("SET_EVENT", response.data);
+        })
+        .catch(error => {
+          const notification = {
+            type: "error",
+            message: "There was a problem fetching event" + error.message
+          };
+
+          dispatch("notification/add", notification, { root: true });
+        });
+    }
+  }
+};
+export const getters = {
+  getEventById: state => id => {
+    return state.events.find(event => event.id === id);
+  }
+};
--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,17 @@
 import Vue from "vue";
 import Vuex from "vuex";
 /* Star is importing all public items into user namespace */
-import * as user from "@/modules/user.js";
-import * as event from "@/modules/event.js";
+import * as user from "@/store/modules/user.js";
+import * as event from "@/store/modules/event.js";
+import * as notification from "@/store/modules/notification.js";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   /*modules we are telling vuex to use that module */
   modules: {
     user,
-    event
+    event,
+    notification
   },
   state: {
     categories: [
